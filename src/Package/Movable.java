cbb2054bd9e999aceae2960cd8cc7b7a3740ddac
Любовь <lubov@MacBook-Air-Lubov.local>
2024-12-02 11:56:58 +0300

    Initial commit

---
+++ src/Package/Movable.java
@@ -0,0 +1,9 @@
+package Package;
+
+public interface Movable {
+
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
---
+++ src/Package/MovablePoint.java
@@ -0,0 +1,30 @@
+package Package;
+
+public class MovablePoint extends Movable{
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+
+    @Override
+    public void moveUp(){
+
+    }
+}
